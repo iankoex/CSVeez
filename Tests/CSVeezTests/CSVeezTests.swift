c78c3b9a04bffc9bfafd6028bda9736211e6e7a7
iankoex <koexian@gmail.com>
2023-03-21 16:42:23 +0300

    swift Files

---
+++ Tests/CSVeezTests/CSVeezTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CSVeez
+
+final class CSVeezTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CSVeez().text, "Hello, World!")
+    }
+}
